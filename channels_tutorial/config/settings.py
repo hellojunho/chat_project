5efc00e476ca8e05f9ca3dd062d60e5047dea797
hellojunho <jo62698476@gmail.com>
2024-08-08 10:32:52 +0900

    refactor: Apply CORS

--- channels_tutorial/config/settings.py
+++ channels_tutorial/config/settings.py
@@ -25,9 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "hellojunho.shop", "www.hellojunho.shop"]
+
+CSRF_TRUSTED_ORIGINS = ['https://hellojunho.shop', 'https://www.hellojunho.shop']
 
 # Application definition
 
