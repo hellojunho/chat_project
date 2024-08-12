95d43003223520e72ea9d3d20ab4e1efea2e0cc5
hellojunho <jo62698476@gmail.com>
2024-08-13 01:49:27 +0900

    refactor: Deleted test of using utils.py

--- channels_tutorial/chat/tests.py
+++ channels_tutorial/chat/tests.py
@@ -1,25 +1,6 @@
 import os
 import smtplib
 from django.test import TestCase
-from config.utils import send_email
-
-
-class TestSendEmail(TestCase):
-    def test_send_email(self) -> bool:
-        """
-        이메일 전송 테스트
-        param:
-        -
-        return:
-        - 이메일 전송 성공 시 True, 실패 시 False
-        """
-        try:
-            result_to_send_email = send_email("junho991026@naver.com", "test message")
-            self.assertTrue(result_to_send_email)
-            return True
-        except Exception as e:
-            return False
-
 
 class TestSmtp(TestCase):
     def test_smtp(self) -> bool:
